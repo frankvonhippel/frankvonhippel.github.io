4fa31deed51edbb4c24ba219ae8e740f5921f5ff
Max von Hippel <maxvonhippel1996@gmail.com>
2020-09-07 22:37:49 -0700

    Increased all fonts

--- episodes.js
+++ episodes.js
@@ -109,6 +109,41 @@ $(document).ready(function() {
 				podnode.appendChild(card);
 				// append item to list
 				document.getElementById("pods").appendChild(podnode);
+
+				$(description).flowtype({
+			 		minimum   : 500,
+			 		maximum   : 1200,
+			 		minFont   : 35,
+			 		maxFont   : 45
+				});
+
+				$(textnode).flowtype({
+			 		minimum   : 500,
+			 		maximum   : 1200,
+			 		minFont   : 35,
+			 		maxFont   : 45
+				});
+
+				$('.p').flowtype({
+			 		minimum   : 500,
+			 		maximum   : 1200,
+			 		minFont   : 35,
+			 		maxFont   : 45
+				});
+
+				$('.btn').flowtype({
+			 		minimum   : 500,
+			 		maximum   : 1200,
+			 		minFont   : 35,
+			 		maxFont   : 45
+				});
+
+				$('.h1').flowtype({
+			 		minimum   : 500,
+			 		maximum   : 1200,
+			 		minFont   : 40,
+			 		maxFont   : 50
+				});
 				
 				var _src = "//html5-player.libsyn.com/embed/episode/id/"
 						 + id
@@ -127,8 +162,8 @@ $(document).ready(function() {
 				$('body').flowtype({
 			 		minimum   : 500,
 			 		maximum   : 1200,
-			 		minFont   : 25,
-			 		maxFont   : 30
+			 		minFont   : 30,
+			 		maxFont   : 40
 				});
 
 			});
--- pubs.js
+++ pubs.js
@@ -7,12 +7,33 @@ $(document).ready(function() {
 	
 	document.getElementById("NOJS").outerHTML = "";
 
+	$('.p').flowtype({
+ 		minimum   : 500,
+ 		maximum   : 1200,
+ 		minFont   : 35,
+ 		maxFont   : 45
+	});
+
+	$('.btn').flowtype({
+ 		minimum   : 500,
+ 		maximum   : 1200,
+ 		minFont   : 35,
+ 		maxFont   : 45
+	});
+
+	$('.h1').flowtype({
+ 		minimum   : 500,
+ 		maximum   : 1200,
+ 		minFont   : 40,
+ 		maxFont   : 50
+	});
+
 	$('body').flowtype({
  		minimum   : 500,
  		maximum   : 1200,
- 		minFont   : 25,
- 		maxFont   : 30
- 	});
+ 		minFont   : 30,
+ 		maxFont   : 40
+	});
 
 	
 });
